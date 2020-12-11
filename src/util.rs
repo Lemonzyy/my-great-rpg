ef422c3df2e29dbfd97eac8a0abd8805d4323e28
Lemonzyy <lemonzy.pro@gmail.com>
2020-12-12 00:20:40 +0100

    Add a "ask" function which gonna be used instead of all "ask[...]" functions

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,7 @@
-use std::io;
+use std::{
+    env::Args,
+    io::{self, Write},
+};
 
 const SEPARATOR: &str = "====";
 
@@ -21,4 +24,22 @@ pub fn clear_console() {
 
 pub fn print_title(str: &str) {
     println!("{0} {1} {0}", SEPARATOR, str)
+}
+
+pub fn ask<A, C, F>(action: A, condition: C, fallback: F) -> Result<String, String>
+    where A: Fn(),
+          C: Fn(&String) -> bool,
+          F: Fn(&String) {
+    loop {
+        action();
+
+        io::stdout().flush().expect("Error while stdout flushing");
+        let value = read_line();
+
+        if !value.is_empty() && condition(&value) {
+            break Ok(value);
+        } else {
+            fallback(&value)
+        }
+    }
 }
