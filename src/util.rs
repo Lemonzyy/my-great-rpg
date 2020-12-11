25ea43676dc7260e82c3f2543bf47696a1a8e6d2
Lemonzyy <lemonzy.pro@gmail.com>
2020-12-11 19:19:41 +0100

    Add a title printing function with a separator

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,13 @@
 use std::io;
 
+const SEPARATOR: &str = "====";
+
 /// This function use recursion for getting the user console input.
 pub fn read_line() -> String {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(..) => {
-            return input.trim_end().to_string()
+            return input.trim_end().to_string();
         }
         Err(error) => println!("Error during read_line: {}", error),
     }
@@ -15,4 +17,8 @@ pub fn read_line() -> String {
 /// This function clear the console and put the cursor at the top right.
 pub fn clear_console() {
     print!("\x1B[2J\x1B[1;1H");
+}
+
+pub fn print_title(str: &str) {
+    println!("{0} {1} {0}", SEPARATOR, str)
 }
