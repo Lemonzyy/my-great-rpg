70cceede452d00985c67b602e4905b30487ebec1
Lemonzyy <lemonzy.pro@gmail.com>
2020-12-11 17:33:17 +0100

    Fix a problem in the "read_line" function where the last part of the input was not correctly deleted Add "clear_console" function in util Add small documentation for functions in "util.rs"

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,18 @@
 use std::io;
 
+/// This function use recursion for getting the user console input.
 pub fn read_line() -> String {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(..) => {
-            input.truncate(input.len() - 1);
-            return input;
+            return input.trim_end().to_string()
         }
         Err(error) => println!("Error during read_line: {}", error),
     }
     read_line()
+}
+
+/// This function clear the console and put the cursor at the top right.
+pub fn clear_console() {
+    print!("\x1B[2J\x1B[1;1H");
 }
